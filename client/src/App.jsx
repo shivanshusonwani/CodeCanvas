f1d6862a33bc1a2c50b5254b6c91e489ebd57d50
ShivanshuSonwani <shivanshu.ed@gmail.com>
2026-03-31 22:04:16 +0530

    initialize React Vite project with Tailwind, Axios, and React router

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className='h-screen flex justify-center items-center text-6xl font-bold text-sky-300 bg-neutral-800'>
+			<h1>CodeCanvas</h1>
+		</div>
+	);
+}
+
+export default App;
